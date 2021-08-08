71404ee3c310fca5eabfcdff50505b07b2b4a210
ShawDa <1315193735@qq.com>
2021-08-08 13:54:56 +0800

    leetcode 20210818 daily

--- Code/TestMaven/src/main/java/cn/shawda/Solution.java
+++ Code/TestMaven/src/main/java/cn/shawda/Solution.java
@@ -13,6 +13,22 @@ class Solution {
         System.out.println(new Solution().minDays(new int[]{1,10,2,9,3,8,4,7,5,6}, 4, 2));
     }
 
+    public int tribonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        int[] res = new int[n + 1];
+        res[1] = 1;
+        res[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            res[i] = res[i - 3] + res[i - 2] + res[i - 1];
+        }
+        return res[n];
+    }
+
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
